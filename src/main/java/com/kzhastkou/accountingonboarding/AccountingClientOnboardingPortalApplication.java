cfb80c582294bde32def3e0767b9a18525e46ae5
Kiryl Zhastkou <kirtinner@gmail.com>
2026-07-04 05:17:23 +0300

    Initial Spring Boot project setup

---
+++ src/main/java/com/kzhastkou/accountingonboarding/AccountingClientOnboardingPortalApplication.java
@@ -0,0 +1,13 @@
+package com.kzhastkou.accountingonboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingClientOnboardingPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingClientOnboardingPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kzhastkou/accountingonboarding/AccountingClientOnboardingPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kzhastkou.accountingonboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountingClientOnboardingPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
